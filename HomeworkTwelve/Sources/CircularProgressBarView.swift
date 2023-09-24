877f0fe9d3ff1a79131d0a22752f3110c283f1bd
Dmitry Yastrebov <stewielime2018@gmail.com>
2023-09-24 21:43:56 +0300

    Add methods to CircularProgressBarView

--- HomeworkTwelve/Sources/CircularProgressBarView.swift
+++ HomeworkTwelve/Sources/CircularProgressBarView.swift
@@ -28,4 +28,71 @@ class CircularProgressBarView: UIView {
         super.init(coder: aDecoder)
         createCircularPath()
     }
+    
+    // MARK: - Methods
+    
+    private func createCircularPath() {
+        let circularPath = UIBezierPath(
+            arcCenter: CGPoint(x: bounds.width / 2, y: bounds.height / 2),
+            radius: (bounds.width - progressBarWidth) / 2,
+            startAngle: -CGFloat.pi / 2,
+            endAngle: 3 * CGFloat.pi / 2,
+            clockwise: true
+        )
+
+        trackLayer.path = circularPath.cgPath
+        trackLayer.fillColor = UIColor.clear.cgColor
+        trackLayer.strokeColor = trackColor.cgColor
+        trackLayer.lineWidth = progressBarWidth
+        trackLayer.lineCap = .round
+        layer.addSublayer(trackLayer)
+
+        progressLayer.path = circularPath.cgPath
+        progressLayer.fillColor = UIColor.clear.cgColor
+        progressLayer.strokeColor = progressBarColor.cgColor
+        progressLayer.lineWidth = progressBarWidth
+        progressLayer.lineCap = .round
+        progressLayer.strokeEnd = 0
+        layer.addSublayer(progressLayer)
+    }
+
+    func startAnimating(duration: TimeInterval) {
+        self.duration = duration
+        startTime = Date().timeIntervalSinceReferenceDate - pausedTime
+        timer = Timer.scheduledTimer(timeInterval: 0.1,
+                                     target: self,
+                                     selector: #selector(updateProgress),
+                                     userInfo: nil,
+                                     repeats: true)
+        isAnimating = true
+    }
+
+    @objc private func updateProgress() {
+        if isAnimating {
+            let currentTime = Date().timeIntervalSinceReferenceDate - pausedTime
+            let elapsedTime = currentTime - startTime
+            let progress = elapsedTime / duration
+            progressLayer.strokeEnd = CGFloat(min(pausedProgress + progress, 1.0))
+
+            if elapsedTime >= duration {
+                timer?.invalidate()
+                isAnimating = false
+            }
+        }
+    }
+
+    func pauseAnimating() {
+        timer?.invalidate()
+        pausedTime = Date().timeIntervalSinceReferenceDate - startTime
+        pausedProgress = progressLayer.strokeEnd
+        isAnimating = false
+    }
+
+    func stopAnimating() {
+        timer?.invalidate()
+        progressLayer.strokeEnd = 0
+        pausedTime = 0
+        pausedProgress = 0
+        isAnimating = false
+    }
 }
