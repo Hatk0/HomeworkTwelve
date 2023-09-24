023382e7c824e1008a653fa43094aa00097a58e0
Dmitry Yastrebov <stewielime2018@gmail.com>
2023-09-24 21:18:49 +0300

    Add properties to CircularProgressView

--- HomeworkTwelve/Sources/CircularProgressBarView.swift
+++ HomeworkTwelve/Sources/CircularProgressBarView.swift
@@ -2,4 +2,18 @@ import UIKit
 
 class CircularProgressBarView: UIView {
     
+    // MARK: - Properties
+    
+    var progressLayer = CAShapeLayer()
+    private var trackLayer = CAShapeLayer()
+    private var timer: Timer?
+    private var startTime: TimeInterval = 0
+    private var duration: TimeInterval = 0
+    private var pausedTime: TimeInterval = 0
+    private var pausedProgress: CGFloat = 0
+    private var isAnimating = false
+
+    var progressBarWidth: CGFloat = 5
+    var progressBarColor: UIColor = .systemGreen
+    var trackColor: UIColor = UIColor(white: 0.9, alpha: 1.0)
 }
