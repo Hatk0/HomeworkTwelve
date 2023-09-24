f48c0e585f476f9c14ddeacb517a7f91798b352f
Dmitry Yastrebov <stewielime2018@gmail.com>
2023-09-24 11:12:13 +0300

    Project customization

---
+++ HomeworkTwelve/Sources/ViewController.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - UI
+    
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupHierarchy()
+        setupLayout()
+    }
+    
+    // MARK: - Setups
+    
+    private func setupView() {
+        view.backgroundColor = .white
+    }
+    
+    private func setupHierarchy() {
+        
+    }
+    
+    private func setupLayout() {
+        
+    }
+    
+    // MARK: - Actions
+
+}
+
